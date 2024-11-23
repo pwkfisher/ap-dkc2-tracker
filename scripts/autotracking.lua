bea590e4f9579556c6bf861bfb3b5ac1aaeb93c8
pwkfisher <patrick@pwkfisher.com>
2024-11-22 20:19:20 -0600

    Working autotracking functionality

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -1,9 +1,15 @@
+ScriptHost:LoadScript("scripts/autotracking/settings.lua")
+-- loads the AP autotracking code
+
+
 -- Configuration --------------------------------------
 AUTOTRACKER_ENABLE_ITEM_TRACKING = true
 AUTOTRACKER_ENABLE_LOCATION_TRACKING = true and not IS_ITEMS_ONLY
+ENABLE_DEBUG_LOG = true
 AUTOTRACKER_ENABLE_DEBUG_LOGGING = true and ENABLE_DEBUG_LOG
 AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP = true and AUTOTRACKER_ENABLE_DEBUG_LOGGING
 AUTOTRACKER_ENABLE_DEBUG_LOGGING_SNES = true and AUTOTRACKER_ENABLE_DEBUG_LOGGING
+
 -------------------------------------------------------
 print("")
 print("Active Auto-Tracker Configuration")
@@ -18,11 +24,7 @@ end
 print("---------------------------------------------------------------------")
 print("")
 
--- uncomment this to use `Tracker.AllowDeferredLogicUpdate` to defer logic updates, which might be needed if you touch A LOT of items/locations at once.
--- if Tracker.AllowDeferredLogicUpdate == false then Tracker.AllowDeferredLogicUpdate = true end
-
--- loads the AP autotracking code
 ScriptHost:LoadScript("scripts/autotracking/archipelago.lua")
--- loads the SNES autotracking code
-ScriptHost:LoadScript("scripts/autotracking/snes.lua")
 
+
+        
